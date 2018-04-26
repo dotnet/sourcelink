5245046e1cb83bb8c4866451c0f12cb66a8586c0
Tomas Matousek <tomat@microsoft.com>
2018-04-25 19:00:16 -0700

    Add support for STANDARD_CI_REPOSITORY_URL_MAP to VSTS.Git task

--- src/SourceLink.VSTS.Git/GetVstsGitSourceLinkUrl.cs
+++ src/SourceLink.VSTS.Git/GetVstsGitSourceLinkUrl.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Microsoft.Build.Framework;
@@ -11,11 +12,15 @@ namespace SourceLink.VSTS.Git
 {
     public sealed class GetVstsGitSourceLinkUrl : Task
     {
+        private const string UrlMapEnvironmentVariableName = "STANDARD_CI_REPOSITORY_URL_MAP";
+        private const string DefaultDomain = "visualstudio.com";
         private const string SourceControlName = "git";
         private const string NotApplicableValue = "N/A";
 
         [Required]
         public ITaskItem SourceRoot { get; set; }
+
+        public string Domain { get; set; }
 
         [Output]
         public string SourceLinkUrl { get; set; }
@@ -41,35 +46,106 @@ namespace SourceLink.VSTS.Git
                 return false;
             }
 
-            if (!TryParseRepositoryUrl(repoUri, out var projectName, out var repositoryName))
+            var map = TryGetStandardUriMap();
+            if (map != null && map.TryGetValue(repoUri, out var mappedUri))
             {
-                SourceLinkUrl = NotApplicableValue;
-                return true;
+                repoUri = mappedUri;
             }
 
-            bool IsHexDigit(char c)
-                => c >= '0' && c <= '9' || c >= 'a' && c <= 'f' || c >= 'A' && c <= 'F';
-
-            string revisionId = SourceRoot.GetMetadata(Names.SourceRoot.RevisionId);
-            if (revisionId == null || revisionId.Length != 40 || !revisionId.All(IsHexDigit))
+            string domain = string.IsNullOrEmpty(Domain) ? DefaultDomain : Domain;
+            if (!TryParseRepositoryUrl(repoUri, domain, out var projectName, out var repositoryName))
             {
-                Log.LogErrorFromResources("ValueOfWithIdentityIsNotValidCommitHash", Names.SourceRoot.RevisionIdFullName, SourceRoot.ItemSpec, revisionId);
                 return false;
             }
 
-            SourceLinkUrl = $"{repoUri.Scheme}://{repoUri.Host}/{projectName}/_apis/git/repositories/{repositoryName}/items?api-version=1.0&versionType=commit&version={revisionId}&path=/*";
+            var query = GetSourceLinkQuery();
+            if (query == null)
+            {
+                return false;
+            }
+
+            SourceLinkUrl = $"{repoUri.Scheme}://{repoUri.Host}/{projectName}/_apis/git/repositories/{repositoryName}/items?" + query;
             return true;
         }
 
-        private static bool TryParseRepositoryUrl(Uri repoUri, out string projectName, out string repositoryName)
+        private Dictionary<Uri, Uri> TryGetStandardUriMap()
+        {
+            var urlSeparators = new[] { Path.PathSeparator };
+            Dictionary<Uri, Uri> map = null;
+
+            bool parse(string urls)
+            {
+                var items = urls.Split(urlSeparators, StringSplitOptions.None);
+                if (items.Length % 2 != 0)
+                {
+                    return false;
+                }
+
+                for (int i = 0; i < items.Length; i += 2)
+                {
+                    string originalUrl = items[i];
+                    string mappedUrl = items[i + 1];
+
+                    if (!Uri.TryCreate(originalUrl, UriKind.Absolute, out var originalUri) || originalUri.Query != "")
+                    {
+                        return false;
+                    }
+
+                    if (!Uri.TryCreate(mappedUrl, UriKind.Absolute, out var mappedUri) || mappedUri.Query != "")
+                    {
+                        return false;
+                    }
+
+                    if (map == null)
+                    {
+                        map = new Dictionary<Uri, Uri>();
+                    }
+
+                    map[originalUri] = mappedUri;
+                }
+
+                return true;
+            }
+
+            IEnumerable<KeyValuePair<string, string>> enumerateVariables()
+            {
+                int i = 0;
+                while (true)
+                {
+                    var name = UrlMapEnvironmentVariableName + (i == 0 ? "" : i.ToString());
+                    var value = Environment.GetEnvironmentVariable(name);
+                    if (string.IsNullOrEmpty(value))
+                    {
+                        yield break;
+                    }
+
+                    yield return new KeyValuePair<string, string>(name, value);
+                    i++;
+                }
+            }
+
+            foreach (var variable in enumerateVariables())
+            {
+                if (!parse(variable.Value))
+                {
+                    Log.LogErrorFromResources("EnvironmentVariableIsNotAlistOfUrlPairs", variable.Key, variable.Value);
+                    return null;
+                }
+            }
+
+            return map;
+        }
+
+        private static bool TryParseRepositoryUrl(Uri repoUri, string domain, out string projectName, out string repositoryName)
         {
             // URL format pattern:
-            // https://{account}.visualstudio.com/[DefaultCollection/]?{project}/_git/{repository-name}
+            // https://{account}.{domain}/[DefaultCollection/]?{project}/_git/{repository-name}
 
             projectName = null;
             repositoryName = null;
 
-            if (!repoUri.Host.EndsWith(".visualstudio.com", StringComparison.OrdinalIgnoreCase))
+            if (!repoUri.Host.EndsWith("." + domain, StringComparison.OrdinalIgnoreCase) && 
+                !repoUri.Host.Equals(domain, StringComparison.OrdinalIgnoreCase))
             {
                 return false;
             }
@@ -93,6 +169,21 @@ namespace SourceLink.VSTS.Git
             repositoryName = parts[parts.Length - 1];
             projectName = parts[parts.Length - 3];
             return true;
+        }
+
+        private string GetSourceLinkQuery()
+        {
+            bool IsHexDigit(char c)
+                => c >= '0' && c <= '9' || c >= 'a' && c <= 'f' || c >= 'A' && c <= 'F';
+
+            string revisionId = SourceRoot.GetMetadata(Names.SourceRoot.RevisionId);
+            if (revisionId == null || revisionId.Length != 40 || !revisionId.All(IsHexDigit))
+            {
+                Log.LogErrorFromResources("ValueOfWithIdentityIsNotValidCommitHash", Names.SourceRoot.RevisionIdFullName, SourceRoot.ItemSpec, revisionId);
+                return null;
+            }
+
+            return $"api-version=1.0&versionType=commit&version={revisionId}&path=/*";
         }
     }
 }
