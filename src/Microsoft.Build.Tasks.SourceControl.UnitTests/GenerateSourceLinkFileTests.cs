834be57ea2a03caa415e85eeeecef38dc317bafd
Tomas Matousek <tomat@microsoft.com>
2018-05-02 13:13:59 -0700

    Remove unused namespace

--- src/Microsoft.Build.Tasks.SourceControl.UnitTests/GenerateSourceLinkFileTests.cs
+++ src/Microsoft.Build.Tasks.SourceControl.UnitTests/GenerateSourceLinkFileTests.cs
@@ -1,5 +1,4 @@
 ﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
-using Microsoft.Build.Tasks.Git.UnitTests;
 using System;
 using System.IO;
 using Xunit;
