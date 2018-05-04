a2ac3fbbd63e390e77d70243c7dae786296d9108
Tomas Matousek <tomat@microsoft.com>
2018-05-04 10:43:39 -0700

    Add integration test project

---
+++ src/SourceLink.Git.IntegrationTests/Tests.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+using Xunit;
+
+namespace Microsoft.Build.Tasks.SourceControl.UnitTests
+{
+    public class PathUtilitiesTests
+    {
+        [Fact]
+        public void Test()
+        {
+            
+        }
+    }
+}
