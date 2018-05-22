b3a4443201ef7b4ba9de1dc23b89346ee1c75a68
Tomas Matousek <tomat@microsoft.com>
2018-05-22 16:04:42 -0700

    Remove unnecessary net461 libs and implement AssemblyResolve event.
    AssemblyResolve is needed to load dependencies of netstandard.dll since their version 0.0.0.0 needs to be mapped to the actual assembly version.

--- src/Microsoft.Build.Tasks.Git/TaskImplementation.cs
+++ src/Microsoft.Build.Tasks.Git/TaskImplementation.cs
@@ -14,14 +14,21 @@ namespace Microsoft.Build.Tasks.Git
         public static Func<GetSourceRoots, bool> GetSourceRoots;
         public static Func<GetUntrackedFiles, bool> GetUntrackedFiles;
 
+        private static readonly string s_taskDirectory;
+        private const string GitOperationsAssemblyName = "Microsoft.Build.Tasks.Git.Operations";
+        private static Version s_nullVersion = new Version(0, 0, 0, 0);
+
         static TaskImplementation()
         {
+            s_taskDirectory = Path.GetDirectoryName(typeof(TaskImplementation).Assembly.Location);
 #if NET461
+            AppDomain.CurrentDomain.AssemblyResolve += AssemblyResolve;
+
             var assemblyName = typeof(TaskImplementation).Assembly.GetName();
-            assemblyName.Name = "Microsoft.Build.Tasks.Git.Operations";
+            assemblyName.Name = GitOperationsAssemblyName;
             var assembly = Assembly.Load(assemblyName);
 #else
-            var operationsPath = Path.Combine(Path.GetDirectoryName(typeof(TaskImplementation).Assembly.Location), "Microsoft.Build.Tasks.Git.Operations.dll");
+            var operationsPath = Path.Combine(s_taskDirectory, GitOperationsAssemblyName + ".dll");
             var assembly = GitLoaderContext.Instance.LoadFromAssemblyPath(operationsPath);
 #endif
             var type = assembly.GetType("Microsoft.Build.Tasks.Git.RepositoryTasks", throwOnError: true).GetTypeInfo();
@@ -32,5 +39,26 @@ namespace Microsoft.Build.Tasks.Git
             GetSourceRoots = (Func<GetSourceRoots, bool>)type.GetDeclaredMethod(nameof(GetSourceRoots)).CreateDelegate(typeof(Func<GetSourceRoots, bool>));
             GetUntrackedFiles = (Func<GetUntrackedFiles, bool>)type.GetDeclaredMethod(nameof(GetUntrackedFiles)).CreateDelegate(typeof(Func<GetUntrackedFiles, bool>));
         }
+
+#if NET461
+        private static Assembly AssemblyResolve(object sender, ResolveEventArgs args)
+        {
+            // only resolve dependencies of netstandard library:
+            if (!StringComparer.OrdinalIgnoreCase.Equals(args.RequestingAssembly.FullName, "netstandard, Version=2.0.0.0, Culture=neutral, PublicKeyToken=cc7b13ffcd2ddd51"))
+            {
+                return null;
+            }
+
+            var referenceName = new AssemblyName(AppDomain.CurrentDomain.ApplyPolicy(args.Name));
+            if (referenceName.Version != s_nullVersion)
+            {
+                return null;
+            }
+
+            // resolve dependencies in the task's directory:
+            var referencePath = Path.Combine(s_taskDirectory, referenceName.Name + ".dll");
+            return File.Exists(referencePath) ? Assembly.Load(referencePath) : null;
+        }
+#endif
     }
 }
