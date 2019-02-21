ac86bfa0302c9fad15f4ebdb3c1daba64fcba5fb
Adam Sajdak <adam.sajdak@solarwinds.com>
2019-02-21 16:02:19 +0100

    Add support for Bitbucket Enterprise

--- src/SourceLink.Bitbucket.Git/GetSourceLinkUrl.cs
+++ src/SourceLink.Bitbucket.Git/GetSourceLinkUrl.cs
@@ -16,7 +16,84 @@ namespace Microsoft.SourceLink.Bitbucket.Git
         protected override string HostsItemGroupName => "SourceLinkBitbucketGitHost";
         protected override string ProviderDisplayName => "Bitbucket.Git";
 
-        protected override string BuildSourceLinkUrl(Uri contentUri, Uri gitUri, string relativeUrl, string revisionId, ITaskItem hostItem)
-            => UriUtilities.Combine(UriUtilities.Combine(contentUri.ToString(), relativeUrl), "raw/" + revisionId + "/*");
+        private const string IsEnterpriseEditionMetadataName = "EnterpriseEdition";
+        private const string VersionMetadataName = "Version";
+        private const string VersionWithNewUrlFormat = "4.7";
+
+        protected override bool SupportsImplicitHost => false;
+
+        protected override string BuildSourceLinkUrl(Uri contentUri, Uri gitUri, string relativeUrl, string revisionId,
+            ITaskItem hostItem)
+        {
+
+           var isEnterpriseEditionFlagAvailable =
+                bool.TryParse(hostItem.GetMetadata(IsEnterpriseEditionMetadataName), out var isEnterpriseEdition);
+
+            if (isEnterpriseEditionFlagAvailable && isEnterpriseEdition)
+            {
+                return BuildSourceLinkUrlForEnterpriseEdition(contentUri, relativeUrl, revisionId, hostItem);
+            }
+            else
+            {
+                return BuildSourceLinkUrlForCloudEdition(contentUri, relativeUrl, revisionId);
+            }
+        }
+
+        private string BuildSourceLinkUrlForEnterpriseEdition(Uri contentUri, string relativeUrl, string revisionId,
+            ITaskItem hostItem)
+        {
+            var bitbucketEnterpriseVersion = GetBitbucketEnterpriseVersion(hostItem);
+
+            var splits = relativeUrl.Split(new[] {'/'}, StringSplitOptions.RemoveEmptyEntries);
+            var relativeUrlForBitbucketEnterprise =
+                GetRelativeUrlForBitbucketEnterprise(splits[0], splits[1], revisionId, bitbucketEnterpriseVersion);
+
+            var result = UriUtilities.Combine(contentUri.ToString(), relativeUrlForBitbucketEnterprise);
+
+            return result;
+        }
+
+        private Version GetBitbucketEnterpriseVersion(ITaskItem hostItem)
+        {
+            var bitbucketEnterpriseVersionAsString = hostItem.GetMetadata(VersionMetadataName);
+            Version bitbucketEnterpriseVersion;
+            if (!string.IsNullOrEmpty(bitbucketEnterpriseVersionAsString))
+            {
+                if (!Version.TryParse(bitbucketEnterpriseVersionAsString, out bitbucketEnterpriseVersion))
+                {
+                    Log.LogError(CommonResources.ItemOfItemGroupMustSpecifyMetadata, hostItem.ItemSpec,
+                        HostsItemGroupName, VersionMetadataName);
+
+                    return null;
+                }
+            }
+            else
+            {
+                bitbucketEnterpriseVersion = Version.Parse(VersionWithNewUrlFormat);
+            }
+
+            return bitbucketEnterpriseVersion;
+        }
+
+        private static string BuildSourceLinkUrlForCloudEdition(Uri contentUri, string relativeUrl, string revisionId)
+        {
+            return UriUtilities.Combine(UriUtilities.Combine(contentUri.ToString(), relativeUrl),
+                "raw/" + revisionId + "/*");
+        }
+
+        private static string GetRelativeUrlForBitbucketEnterprise(string projectName, string repositoryName, string commitId, Version bitbucketVersion)
+        {
+            string relativeUrl;
+            if (bitbucketVersion >= Version.Parse(VersionWithNewUrlFormat))
+            {
+                relativeUrl = $"projects/{projectName}/repos/{repositoryName}/raw/*?at={commitId}";
+            }
+            else
+            {
+                relativeUrl = $"projects/{projectName}/repos/{repositoryName}/browse/*?at={commitId}&raw";
+            }
+
+            return relativeUrl;
+        }
     }
 }
