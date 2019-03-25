594c08c82a29c0f5cc1b043dec87f56af05a60fe
Tomáš Matoušek <tmat@users.noreply.github.com>
2019-03-25 09:00:34 -0700

    Arcade 1.0.0-beta.19167.10 (#257)

--- eng/CIBuild.cmd
+++ eng/CIBuild.cmd
@@ -1,3 +1,3 @@
 @echo off
-powershell -ExecutionPolicy ByPass -command "& """%~dp0common\Build.ps1""" -restore -build -test -sign -pack -ci -integrationTest %*"
+powershell -ExecutionPolicy ByPass -command "& """%~dp0common\Build.ps1""" -restore -build -test -sign -pack -ci -publish -integrationTest %*"
 exit /b %ErrorLevel%
