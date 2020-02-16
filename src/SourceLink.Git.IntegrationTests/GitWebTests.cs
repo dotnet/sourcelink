bdc85b7525cfa0f0b918891b5e07f5a1306c5bf8
Glen Nicol <58054391+Glen-Nicol-Garmin@users.noreply.github.com>
2020-02-16 12:41:06 -0800

    Feature: GitWeb Source Link provider (#505)
    * Feature: GitWeb Source Link provider
    
    GitWeb only currently supports SSH repository URLs.
    
    https://github.com/dotnet/sourcelink/pull/505
    
    * Remove Resources.Designer.cs
    
    * Remove comment
    
    Co-authored-by: Tomáš Matoušek <tmat@users.noreply.github.com>

---
+++ src/SourceLink.Git.IntegrationTests/GitWebTests.cs
@@ -0,0 +1,130 @@
+﻿// Copyright (c) Microsoft. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+// License.txt in the project root for license information.
+
+using Microsoft.Build.Tasks.SourceControl;
+using System;
+using System.IO;
+using TestUtilities;
+
+namespace Microsoft.SourceLink.IntegrationTests
+{
+    public class GitWebTests : DotNetSdkTestBase
+    {
+        public GitWebTests()
+            : base("Microsoft.SourceLink.GitWeb")
+        {
+        }
+
+        [ConditionalFact(typeof(DotNetSdkAvailable))]
+        public void FullValidation_Ssh()
+        {
+            // Test non-ascii characters and escapes in the URL. Escaped URI reserved characters
+            // should remain escaped, non-reserved characters unescaped in the results.
+            var repoUrl = $"ssh://git@噸.com/test-%72epo\u1234%24%2572%2F.git";
+            var repoName = "test-repo\u1234%24%2572%2F.git";
+
+            var repo = GitUtilities.CreateGitRepositoryWithSingleCommit(ProjectDir.Path, new[] { ProjectFileName }, repoUrl);
+            var commitSha = repo.Head.Tip.Sha;
+
+            VerifyValues(
+                customProps: @"
+<PropertyGroup>
+  <PublishRepositoryUrl>true</PublishRepositoryUrl>
+</PropertyGroup>
+<ItemGroup>
+  <SourceLinkGitWebHost Include='噸.com' ContentUrl='https://噸.com/gitweb'/>
+</ItemGroup>
+",
+                customTargets: "",
+                targets: new[]
+                {
+                    "Build", "Pack"
+                },
+                expressions: new[]
+                {
+                    "@(SourceRoot)",
+                    "@(SourceRoot->'%(SourceLinkUrl)')",
+                    "$(SourceLink)",
+                    "$(PrivateRepositoryUrl)",
+                    "$(RepositoryUrl)"
+                },
+                expectedResults: new[]
+                {
+                    ProjectSourceRoot,
+                    $"https://噸.com/gitweb/?p={repoName};a=blob_plain;hb={commitSha};f=*",
+                    s_relativeSourceLinkJsonPath,
+                    $"ssh://git@噸.com/{repoName}",
+                    $"ssh://git@噸.com/{repoName}"
+                });
+
+            AssertEx.AreEqual(
+                $@"{{""documents"":{{""{ProjectSourceRoot.Replace(@"\", @"\\")}*"":""https://噸.com/gitweb/?p={repoName};a=blob_plain;hb={commitSha};f=*""}}}}",
+                File.ReadAllText(Path.Combine(ProjectDir.Path, s_relativeSourceLinkJsonPath)));
+
+            TestUtilities.ValidateAssemblyInformationalVersion(
+                Path.Combine(ProjectDir.Path, s_relativeOutputFilePath),
+                "1.0.0+" + commitSha);
+
+            TestUtilities.ValidateNuSpecRepository(
+                Path.Combine(ProjectDir.Path, s_relativePackagePath),
+                type: "git",
+                commit: commitSha,
+                url: $"ssh://git@噸.com/{repoName}");
+        }
+
+        [ConditionalFact(typeof(DotNetSdkAvailable))]
+        public void Issues_error_on_git_url()
+        {
+            var repoUrl = "git://噸.com/invalid_url_protocol.git";
+            var repo = GitUtilities.CreateGitRepositoryWithSingleCommit(ProjectDir.Path, new[] { ProjectFileName }, repoUrl);
+            var commitSha = repo.Head.Tip.Sha;
+
+            VerifyValues(
+                customProps: @"
+<PropertyGroup>
+  <PublishRepositoryUrl>true</PublishRepositoryUrl>
+</PropertyGroup>
+<ItemGroup>
+  <SourceLinkGitWebHost Include='噸.com' ContentUrl='https://噸.com/gitweb'/>
+</ItemGroup>
+",
+                customTargets: "",
+                targets: new[]
+                {
+                    "Build", "Pack"
+                },
+                expressions: Array.Empty<string>(),
+                expectedErrors: new[]{
+                    string.Format(GitWeb.Resources.RepositoryUrlIsNotSupportedByProvider, "GIT")
+                });
+        }
+
+        [ConditionalFact(typeof(DotNetSdkAvailable))]
+        public void Issues_error_on_https_url()
+        {
+            var repoUrl = "https://噸.com/invalid_url_protocol.git";
+            var repo = GitUtilities.CreateGitRepositoryWithSingleCommit(ProjectDir.Path, new[] { ProjectFileName }, repoUrl);
+            var commitSha = repo.Head.Tip.Sha;
+
+            VerifyValues(
+                customProps: @"
+<PropertyGroup>
+  <PublishRepositoryUrl>true</PublishRepositoryUrl>
+</PropertyGroup>
+<ItemGroup>
+  <SourceLinkGitWebHost Include='噸.com' ContentUrl='https://噸.com/gitweb'/>
+</ItemGroup>
+",
+                customTargets: "",
+                targets: new[]
+                {
+                    "Build", "Pack"
+                },
+                expressions: Array.Empty<string>(),
+                expectedErrors: new[]
+                {
+                    string.Format(GitWeb.Resources.RepositoryUrlIsNotSupportedByProvider, "HTTP")
+                });
+        }
+    }
+}
---
+++ src/SourceLink.GitWeb.UnitTests/TranslateRepositoryUrlsTests.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Microsoft. All Rights Reserved. Licensed under the Apache License, Version 2.0. See
+// License.txt in the project root for license information.
+using System.Linq;
+using TestUtilities;
+using Xunit;
+using static TestUtilities.KeyValuePairUtils;
+
+namespace Microsoft.SourceLink.GitWeb.UnitTests
+{
+    public class TranslateRepositoryUrlsTests
+    {
+        [Fact]
+        public void Translate()
+        {
+            var engine = new MockEngine();
+
+            var task = new TranslateRepositoryUrls()
+            {
+                BuildEngine = engine,
+                RepositoryUrl = "ssh://git@src.intranet.company.com/root_dir_name/sub_dirs/reponame.git",
+                IsSingleProvider = true,
+                SourceRoots = new[]
+                {
+                    new MockItem("/1/", KVP("SourceControl", "git"),  KVP("ScmRepositoryUrl", "ssh://git@src.intranet.company.com/root_dir_name/sub_dirs/reponame.git")),
+                    new MockItem("/2/", KVP("SourceControl", "tfvc"), KVP("ScmRepositoryUrl", "ssh://git@src.intranet.company1.com/root_dir_name/sub_dirs/reponame.git")),
+                    new MockItem("/2/", KVP("SourceControl", "git"),  KVP("ScmRepositoryUrl", "ssh://git@src.intranet.company1.com/root_dir_name/sub_dirs/reponame.git")),
+                    new MockItem("/2/", KVP("SourceControl", "tfvc"), KVP("ScmRepositoryUrl", "ssh://git@src.intranet.company2.com/root_dir_name/sub_dirs/reponame.git")),
+                },
+                Hosts = new[]
+                {
+                    new MockItem("src.intranet.company1.com")
+                }
+            };
+
+            bool result = task.Execute();
+            AssertEx.AssertEqualToleratingWhitespaceDifferences("", engine.Log);
+
+            AssertEx.AreEqual("ssh://git@src.intranet.company.com/root_dir_name/sub_dirs/reponame.git", task.TranslatedRepositoryUrl);
+
+            AssertEx.Equal(new[]
+            {
+                 "ssh://git@src.intranet.company.com/root_dir_name/sub_dirs/reponame.git",
+                 "ssh://git@src.intranet.company1.com/root_dir_name/sub_dirs/reponame.git",
+                 "ssh://git@src.intranet.company1.com/root_dir_name/sub_dirs/reponame.git",
+                 "ssh://git@src.intranet.company2.com/root_dir_name/sub_dirs/reponame.git",
+            }, task.TranslatedSourceRoots.Select(r => r.GetMetadata("ScmRepositoryUrl")));
+
+            Assert.True(result);
+        }
+    }
+}
