93bb565b655080a6f5a5fbf222fdb63da09a61c7
Kai Leonhardt <8343141+Mik4sa@users.noreply.github.com>
2020-11-18 13:42:15 +0100

    Added support for gitea

---
+++ src/SourceLink.Gitea.UnitTests/TranslateRepositoryUrlsTests.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+using System.Linq;
+using TestUtilities;
+using Xunit;
+using static TestUtilities.KeyValuePairUtils;
+
+namespace Microsoft.SourceLink.Gitea.UnitTests
+{
+    public class TranslateRepositoryUrlsTests
+    {
+        [Fact]
+        public void Translate()
+        {
+            var engine = new MockEngine();
+
+            var task = new TranslateRepositoryUrls()
+            {
+                BuildEngine = engine,
+                RepositoryUrl = "ssh://gitea.com/a/b",
+                IsSingleProvider = true,
+                SourceRoots = new[]
+                {
+                    new MockItem("/1/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://gitea.com:22/a/b")),
+                    new MockItem("/2/", KVP("SourceControl", "tfvc"), KVP("ScmRepositoryUrl", "ssh://gitea1.com/a/b")),
+                    new MockItem("/2/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://gitea1.com/a/b")),
+                    new MockItem("/2/", KVP("SourceControl", "tfvc"), KVP("ScmRepositoryUrl", "ssh://gitea2.com/a/b")),
+                },
+                Hosts = new[]
+                {
+                    new MockItem("gitea1.com")
+                }
+            };
+
+            bool result = task.Execute();
+            AssertEx.AssertEqualToleratingWhitespaceDifferences("", engine.Log);
+
+            AssertEx.AreEqual("https://gitea.com/a/b", task.TranslatedRepositoryUrl);
+
+            AssertEx.Equal(new[] 
+            {
+                "https://gitea.com/a/b",
+                "ssh://gitea1.com/a/b",
+                "https://gitea1.com/a/b",
+                "ssh://gitea2.com/a/b"
+            }, task.TranslatedSourceRoots?.Select(r => r.GetMetadata("ScmRepositoryUrl")));
+
+            Assert.True(result);
+        }
+    }
+}
---
+++ src/SourceLink.Gitea/GetSourceLinkUrl.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using System;
+using Microsoft.Build.Framework;
+using Microsoft.Build.Tasks.SourceControl;
+
+namespace Microsoft.SourceLink.Gitea
+{
+    /// <summary>
+    /// The task calculates SourceLink URL for a given SourceRoot.
+    /// If the SourceRoot is associated with a git repository with a recognized domain the <see cref="SourceLinkUrl"/>
+    /// output property is set to the content URL corresponding to the domain, otherwise it is set to string "N/A".
+    /// </summary>
+    public sealed class GetSourceLinkUrl : GetSourceLinkUrlGitTask
+    {
+        protected override string HostsItemGroupName => "SourceLinkGiteaHost";
+        protected override string ProviderDisplayName => "Gitea";
+
+        protected override string? BuildSourceLinkUrl(Uri contentUri, Uri gitUri, string relativeUrl, string revisionId, ITaskItem? hostItem)
+            => UriUtilities.Combine(UriUtilities.Combine(contentUri.ToString(), relativeUrl), "raw/commit/" + revisionId + "/*");
+    }
+}
