21a48ed8ccc928dfbd89755bcedd41408f83ae29
Shreyas Jejurkar <shreyasjejurkar123@live.com>
2022-10-22 06:32:06 +0000

    refactor : change from `build.sh` to `restore.sh`

--- .devcontainer/scripts/container-creation.sh
+++ .devcontainer/scripts/container-creation.sh
@@ -5,7 +5,7 @@ set -e
 # Install SDK and tool dependencies before container starts
 # Also run the full restore on the repo so that go-to definition
 # and other language features will be available in C# files
-./build.sh
+./restore.sh
 
 # The container creation script is executed in a new Bash instance
 # so we exit at the end to avoid the creation process lingering.
