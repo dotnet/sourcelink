c9199d81c03166e2fb0b4e4758c1ceb41b3c1e1f
rosen <black_matrix_7@hotmail.com>
2023-01-09 01:54:10 +0800

    Add support for Gitee (#922)
    * Add support for Gitee
    
    * update unit test for Gitee
    
    * update repository url translate test for Gitee
    
    * fix gitee task 'AssemblyFile' property
    
    * add integration tests for Gitee

---
+++ src/SourceLink.Gitee.UnitTests/GetSourceLinkUrlTests.cs
@@ -0,0 +1,57 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the License.txt file in the project root for more information.
+using Microsoft.Build.Tasks.SourceControl;
+using TestUtilities;
+using Xunit;
+using static TestUtilities.KeyValuePairUtils;
+
+namespace Microsoft.SourceLink.Gitee.UnitTests
+{
+    public class GetSourceLinkUrlTests
+    {
+        [Fact]
+        public void EmptyHosts()
+        {
+            var engine = new MockEngine();
+
+            var task = new GetSourceLinkUrl()
+            {
+                BuildEngine = engine,
+                SourceRoot = new MockItem("x", KVP("RepositoryUrl", "http://abc.com"), KVP("SourceControl", "git")),
+            };
+
+            bool result = task.Execute();
+
+            AssertEx.AssertEqualToleratingWhitespaceDifferences(
+                "ERROR : " + string.Format(CommonResources.AtLeastOneRepositoryHostIsRequired, "SourceLinkGiteeHost", "Gitee"), engine.Log);
+
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData("", "")]
+        [InlineData("", "/")]
+        [InlineData("/", "")]
+        [InlineData("/", "/")]
+        public void BuildSourceLinkUrl(string s1, string s2)
+        {
+            var engine = new MockEngine();
+
+            var task = new GetSourceLinkUrl()
+            {
+                BuildEngine = engine,
+                SourceRoot = new MockItem("/src/", KVP("RepositoryUrl", "http://subdomain.mygitee.com:100/a/b" + s1), KVP("SourceControl", "git"), KVP("RevisionId", "0123456789abcdefABCDEF000000000000000000")),
+                Hosts = new[]
+                {
+                    new MockItem("mygitee.com", KVP("ContentUrl", "https://domain.com/x/y" + s2)),
+                }
+            };
+
+            bool result = task.Execute();
+            AssertEx.AssertEqualToleratingWhitespaceDifferences("", engine.Log);
+            AssertEx.AreEqual("https://domain.com/x/y/a/b/raw/0123456789abcdefABCDEF000000000000000000/*", task.SourceLinkUrl);
+            Assert.True(result);
+        }
+    }
+}
