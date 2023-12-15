9bbf66baca283f495b326210fd150a9c2d04bc1e
Tomáš Matoušek <tmat@users.noreply.github.com>
2023-12-15 13:32:59 -0800

    Refactor common files (#1162)

--- src/Common/Utilities/PathUtilities.cs
+++ src/Common/Utilities/PathUtilities.cs
@@ -5,12 +5,15 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 
 namespace Microsoft.Build.Tasks.SourceControl
 {
     internal static class PathUtilities
     {
+        internal static bool IsUnixLikePlatform => Path.DirectorySeparatorChar == '/';
+
         private static readonly char[] s_directorySeparators = { Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar };
         private const string UncPrefix = @"\\";
         private const string UnixRoot = "/";
@@ -49,5 +52,49 @@ namespace Microsoft.Build.Tasks.SourceControl
 
         public static string EndWithSeparator(this string path, char separator)
             => path.EndsWithSeparator() ? path : path + separator;
+
+        /// <summary>
+        /// True if the path is an absolute path (rooted to drive or network share)
+        /// </summary>
+        public static bool IsAbsolute([NotNullWhen(true)] string? path)
+        {
+            if (path is not { Length: >0 })
+            {
+                return false;
+            }
+
+            if (IsUnixLikePlatform)
+            {
+                return path[0] == Path.DirectorySeparatorChar;
+            }
+
+            // "C:\"
+            if (IsDriveRootedAbsolutePath(path))
+            {
+                // Including invalid paths (e.g. "*:\")
+                return true;
+            }
+
+            // "\\machine\share"
+            // Including invalid/incomplete UNC paths (e.g. "\\goo")
+            return path.Length >= 2 &&
+                IsDirectorySeparator(path[0]) &&
+                IsDirectorySeparator(path[1]);
+        }
+
+        /// <summary>
+        /// True if the character is the platform directory separator character or the alternate directory separator.
+        /// </summary>
+        public static bool IsDirectorySeparator(char c)
+            => c == Path.DirectorySeparatorChar || c == Path.AltDirectorySeparatorChar;
+
+        /// <summary>
+        /// Returns true if given path is absolute and starts with a drive specification ("C:\").
+        /// </summary>
+        private static bool IsDriveRootedAbsolutePath(string path)
+        {
+            Debug.Assert(!IsUnixLikePlatform);
+            return path.Length >= 3 && path[1] == Path.VolumeSeparatorChar && IsDirectorySeparator(path[2]);
+        }
     }
 }
--- src/Common/Utilities/UriUtilities.cs
+++ src/Common/Utilities/UriUtilities.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Linq;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
 
 namespace Microsoft.Build.Tasks.SourceControl
 {
@@ -80,5 +81,77 @@ namespace Microsoft.Build.Tasks.SourceControl
 
         public static string GetPathAndQuery(this Uri uri)
             => uri.GetComponents(UriComponents.PathAndQuery, UriFormat.SafeUnescaped);
+
+        /// <summary>
+        /// Converts an absolute local file path or an absolute URL string to <see cref="Uri"/>.
+        /// </summary>
+        /// <exception cref="UriFormatException">
+        /// The <paramref name="absolutePath"/> can't be represented as <see cref="Uri"/>.
+        /// For example, UNC paths with invalid characters in server name.
+        /// </exception>
+        public static Uri CreateAbsoluteUri(string absolutePath)
+        {
+            var uriString = IsAscii(absolutePath) ? absolutePath : GetAbsoluteUriString(absolutePath);
+            try
+            {
+#pragma warning disable RS0030 // Do not use banned APIs
+                return new(uriString, UriKind.Absolute);
+#pragma warning restore
+
+            }
+            catch (UriFormatException e)
+            {
+                // The standard URI format exception does not include the failing path, however
+                // in pretty much all cases we need to know the URI string (and original string) in order to fix the issue.
+                throw new UriFormatException($"Failed create URI from '{uriString}'; original string: '{absolutePath}'", e);
+            }
+        }
+
+        // Implements workaround for https://github.com/dotnet/runtime/issues/89538:
+        internal static string GetAbsoluteUriString(string absolutePath)
+        {
+            if (!PathUtilities.IsAbsolute(absolutePath))
+            {
+                return absolutePath;
+            }
+
+            var parts = absolutePath.Split([Path.DirectorySeparatorChar, Path.AltDirectorySeparatorChar]);
+
+            if (PathUtilities.IsUnixLikePlatform)
+            {
+                // Unix path: first part is empty, all parts should be escaped
+                return "file://" + string.Join("/", parts.Select(EscapeUriPart));
+            }
+
+            if (parts is ["", "", var serverName, ..])
+            {
+                // UNC path: first non-empty part is server name and shouldn't be escaped
+                return "file://" + serverName + "/" + string.Join("/", parts.Skip(3).Select(EscapeUriPart));
+            }
+
+            // Drive-rooted path: first part is "C:" and shouldn't be escaped
+            return "file:///" + parts[0] + "/" + string.Join("/", parts.Skip(1).Select(EscapeUriPart));
+
+#pragma warning disable SYSLIB0013 // Type or member is obsolete
+            static string EscapeUriPart(string stringToEscape)
+                => Uri.EscapeUriString(stringToEscape).Replace("#", "%23");
+#pragma warning restore
+        }
+
+        private static bool IsAscii(char c)
+            => (uint)c <= '\x007f';
+
+        private static bool IsAscii(string filePath)
+        {
+            for (var i = 0; i < filePath.Length; i++)
+            {
+                if (!IsAscii(filePath[i]))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
