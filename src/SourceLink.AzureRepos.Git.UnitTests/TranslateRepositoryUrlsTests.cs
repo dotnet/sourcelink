f4beeb50ef5e52f5ae799f2291f5bd6271c5da26
Tomáš Matoušek <tmat@users.noreply.github.com>
2026-08-11 11:47:21 -0700

    Strip user name and password from repository URLs

--- src/SourceLink.AzureRepos.Git.UnitTests/TranslateRepositoryUrlsTests.cs
+++ src/SourceLink.AzureRepos.Git.UnitTests/TranslateRepositoryUrlsTests.cs
@@ -18,21 +18,21 @@ namespace Microsoft.SourceLink.AzureRepos.Git.UnitTests
             var task = new TranslateRepositoryUrls()
             {
                 BuildEngine = engine,
-                RepositoryUrl = "ssh://account@vs-ssh.visualstudio.com/project/team/_ssh/repo",
+                RepositoryUrl = "ssh://vs-ssh.visualstudio.com/v3/account/project/team/repo",
                 IsSingleProvider = true,
                 SourceRoots = new[]
                 {
-                    new MockItem("/1/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://account@vs-ssh.visualstudio.com:22/project/team/_ssh/repo")),    // ok
-                    new MockItem("/2/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://test@vs-ssh.visualstudio.com:22/project/_ssh/repo")),            // ok
+                    new MockItem("/1/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://vs-ssh.visualstudio.com:22/v3/account/project/team/repo")),      // ok
+                    new MockItem("/2/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://user@vs-ssh.visualstudio.com:22/v3/test/project/repo")),      // ok
                     new MockItem("/3/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://user@vs-ssh.visualstudio.com:22/v3/account/project/team/repo")), // ok
-                    new MockItem("/4/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://account@vs-ssh.visualstudio.com/_ssh/repo")),                    // ok
-                    new MockItem("/5/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://account@ssh.contoso.com:22/project/team/_ssh/repo")),            // ok
+                    new MockItem("/4/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://vs-ssh.visualstudio.com/v3/account/project/repo")),              // ok
+                    new MockItem("/5/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://ssh.contoso.com:22/v3/account/project/team/repo")),              // ok
                     new MockItem("/6/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://user@ssh.contoso.com/v3/account/project/team/repo")),            // ok
-                    new MockItem("/7/", KVP("SourceControl", "tfvc"), KVP("ScmRepositoryUrl", "ssh://account@vs-ssh.visualstudio.com:22/project/team/_ssh/repo")),   // different source control
-                    new MockItem("/8/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://account@contoso.com:22/project/team/_ssh/repo")),                // no "vs-ssh." prefix
-                    new MockItem("/9/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://account@vs-ssh.contoso.com:22/project/team/_ssh/repo")),         // known host, but not visualstudio.com
-                    new MockItem("/A/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://account@vs-ssh.contoso2.com:22/project/team/_ssh/repo")),        // unknown host
-                    new MockItem("/B/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://account@vs-ssh.contoso.com:22/project/team/ZZZ/repo")),          // bad format
+                    new MockItem("/7/", KVP("SourceControl", "tfvc"), KVP("ScmRepositoryUrl", "ssh://vs-ssh.visualstudio.com:22/v3/account/project/team/repo")),     // different source control
+                    new MockItem("/8/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://contoso.com:22/v3/account/project/team/repo")),                  // no "vs-ssh." prefix
+                    new MockItem("/9/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://vs-ssh.contoso.com:22/v3/account/project/team/repo")),           // known host, but not visualstudio.com
+                    new MockItem("/A/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://vs-ssh.contoso2.com:22/v3/account/project/team/repo")),          // unknown host
+                    new MockItem("/B/", KVP("SourceControl", "git"), KVP("ScmRepositoryUrl", "ssh://vs-ssh.contoso.com:22/v3/account/project/team/ZZZ/repo")),       // bad format
                 },
                 Hosts = new[]
                 {
@@ -45,19 +45,19 @@ namespace Microsoft.SourceLink.AzureRepos.Git.UnitTests
 
             AssertEx.AreEqual("https://account.visualstudio.com/project/team/_git/repo", task.TranslatedRepositoryUrl);
 
-            AssertEx.Equal(new[] 
+            AssertEx.Equal(new[]
             {
                 "https://account.visualstudio.com/project/team/_git/repo",
                 "https://test.visualstudio.com/project/_git/repo",
                 "https://account.visualstudio.com/project/team/_git/repo",
-                "https://account.visualstudio.com/_git/repo",
+                "https://account.visualstudio.com/project/_git/repo",
                 "https://contoso.com/account/project/team/_git/repo",
                 "https://contoso.com/account/project/team/_git/repo",
-                "ssh://account@vs-ssh.visualstudio.com:22/project/team/_ssh/repo",
-                "ssh://account@contoso.com:22/project/team/_ssh/repo",
-                "ssh://account@vs-ssh.contoso.com:22/project/team/_ssh/repo",
-                "ssh://account@vs-ssh.contoso2.com:22/project/team/_ssh/repo",
-                "ssh://account@vs-ssh.contoso.com:22/project/team/ZZZ/repo"
+                "ssh://vs-ssh.visualstudio.com:22/v3/account/project/team/repo",
+                "ssh://contoso.com:22/v3/account/project/team/repo",
+                "ssh://vs-ssh.contoso.com:22/v3/account/project/team/repo",
+                "ssh://vs-ssh.contoso2.com:22/v3/account/project/team/repo",
+                "ssh://vs-ssh.contoso.com:22/v3/account/project/team/ZZZ/repo"
             }, task.TranslatedSourceRoots?.Select(r => r.GetMetadata("ScmRepositoryUrl")));
 
             Assert.True(result);
